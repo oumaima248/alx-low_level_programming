0eaa420376c12418ef40af029e5206e8c8ef3db5
oumaima <oumairari9@gmail.com>
2023-09-07 10:56:18 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *       write(int fd.const void *buf,size_t count)
+ *
+ *       Return: 1 (not Success)
+ */
+
 int main(void)
 {
-        write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        char quo [] = " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write(1,quo,59);
         return (1);
 }
