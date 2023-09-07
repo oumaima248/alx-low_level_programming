c14e52ff2d0517eaa7a2af1da3a6b055d3857ea1
oumaima <oumairari9@gmail.com>
2023-09-07 07:45:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - this is the main function
- *
- * Return: 0 when successful
+ * main - prints "Programming is like building a multilingual puzzle,followed by a new line.
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
-		 return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return "(0);
 }
