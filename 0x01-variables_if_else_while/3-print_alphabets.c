0475c9d3d6b079ad488c7f1b018de97f9b831097
oumaima <oumairari9@gmail.com>
2023-09-08 11:19:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
 
-	for (low = 'A'; low <= 'Z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	putchar('\n');
 
 	return (0);
