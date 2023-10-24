a23e4e39650b3188561ce921897fc6069a0ff5c3
oumaima248 <oumairari9@gmail.com>
2023-10-24 13:16:58 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - Prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: The number of nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
