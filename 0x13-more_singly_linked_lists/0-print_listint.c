e93ef0e1b882c028b55b78498cd0e26039af9042
oumaima248 <oumairari9@gmail.com>
2023-10-24 13:11:30 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,17 +8,13 @@
  * Return: The number of nodes
  */
 size_t print_listint(const listint_t *h)
-
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-
-	return (nodes);
-
+size_t nodes = 0;
+while (h)
+{
+ nodes++;
+ printf("%d\n", h->n);
+h = h->next;
+}
+return (nodes);
 }
